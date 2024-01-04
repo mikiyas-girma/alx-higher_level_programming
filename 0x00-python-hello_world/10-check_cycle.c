11758bf84431c6c7be19bba07d227d9ff4ec4015
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-01-05 01:22:20 +0300

    c function that checks if linkedlist have a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * check_cycle - checks for a cycle in a linked list
+  *
+  * @head: the first node in the list
+  * Return: 0 if no cycle, 1 if cycle found
+  */
+int check_cycle(listint_t *head)
+{
+	listint_t *s, *;
+	int flag = 0;
+
+	if (head == NULL)
+		return (0);
+
+	s = f = head;
+
+	while (f)
+	{
+		flag++;
+		f = f->next;
+		if (flag % 2 == 0)
+			s = s->next;
+		if (f == s)
+			return (1);
+	}
+
+	return (0);
+}
