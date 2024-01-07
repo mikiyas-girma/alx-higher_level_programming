e73a6d6db282377b88ec9e14060371d043a2d50e
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-01-08 00:25:36 +0300

    prints the number and list of its cmd arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+        exit()
+    num = len(sys.argv)
+    if len(sys.argv) == 2:
+        print("{} argument:".format(num - 1))
+    else:
+        print("{} arguments:".format(num - 1))
+
+    i = 0
+    for word in sys.argv:
+        i += 1
+        if i == 1:
+            continue
+        print("{}: {}".format(i - 1, word))
