0219faa921663d8c419baa55cda8451b332f5256
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-01-11 01:40:42 +0300

    replaces all occurrences of an element by another in a new list.

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list = []
+    for a in my_list:
+        if a == search:
+            list.append(replace)
+        else:
+            list.append(x)
+    return list
