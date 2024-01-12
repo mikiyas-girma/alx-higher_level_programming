f810aea343e9980cc9d4599968259a4a4f6a45fc
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-01-12 21:51:22 +0300

    changes roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+def roman_to_int(roman_string):
+    roman_equivalent = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    s_list = list(roman_string)
+    int_value = 0
+
+    for i in range(len(s_list)):
+        if i < len(s_list) - 1 and
+        roman_equivalent[s_list[i]] < roman_equivalent[s_list[i + 1]]:
+            int_value -= roman_equivalent[s_list[i]]
+        else:
+            int_value += roman_equivalent[s_list[i]]
+
+    return int_value
