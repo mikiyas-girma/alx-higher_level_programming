621bfe5e7cfb1414669be64d48c3f099c4f04683
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-02-03 15:31:37 +0300

    added an instance method that returns the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" the module defines a class square with private instance variable """
+
+
+class Square:
+
+    """ class named Square with private instance variable size
+
+    Args:
+        size (int): a value that initializes the ``__size`` attribute
+
+    Attributes:
+        __size (int): private instance variable
+
+    Raises:
+        TypeError: the ``__size`` is not type of int.
+        ValueError: the ``__size`` must be greater than 0.
+    """
+
+    def __init__(self, size=0):
+        """ function that runs immediately just after object instantation """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ function that returns current area of the square """
+        return self.__size * self.__size
