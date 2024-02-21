bcb03676b2e933c955fd197ef71f7ea18cfc6b4f
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-02-21 17:20:54 +0300

    simple modification

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,6 @@ arguments and returns the int
 
 def add_integer(a, b=98):
     """this function returns the sum of its arguments"""
-
     if not a or (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError('a must be an integer')
     if not isinstance(b, int) and not isinstance(b, float):
