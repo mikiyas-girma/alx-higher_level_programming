74177981cd9525a156bccac49ab2fe5493c5b6cd
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-02-23 12:21:05 +0300

    documented the module and function with docstrings

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+this module contains one simple function say_my_name()
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    this function prints the first name and last name that is passed
+    to it
+    """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
