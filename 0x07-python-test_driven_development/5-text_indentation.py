835bf4a93c9644dd47e95c1b8bd4db71d2eedf6e
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-02-23 15:36:58 +0300

    indent function with its test file

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this module contains one function called ``text_indentation()`` """
+
+
+def text_indentation(text):
+    """the function prints two newlines after if it catches
+    . or : or ? in the text given to the function"""
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    for char in text:
+        if char in ('.', ':', '?'):
+            print(char)
+            print()
+            print()
+        else:
+            print(char, end='')
