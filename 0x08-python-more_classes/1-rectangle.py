720078ff569c5f572b46aa94b5bef8e5bb8f12c6
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-02-28 19:46:10 +0300

    checks for __init__ method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,18 @@ class Rectangle:
     """this is empty rectangle class"""
     def __init__(self, width=0, height=0):
         """initializes width and height for an instance"""
-        self.__width
-        self.__height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
