30dcdee4713de8067ec4f02771e38faa76aaafd1
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-02 23:21:29 +0300

    ppycode style

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.n, tu = (magic_string.__dict__.get('n', 0) + 1, ', BestSchool')
-    return '{:s}{:s}'.format(tu[2:], tu * (magic_string.n - 1))
+    magic_string.n, x = (magic_string.__dict__.get('n', 0) + 1, ', BestSchool')
+    return '{:s}{:s}'.format(x[2:], x * (magic_string.n - 1))
