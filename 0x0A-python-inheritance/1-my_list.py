e496545f205372c3cbf96a734e6e7865e5257955
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-05 01:50:05 +0300

    documented the module and the function inside

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""a simple module with one function inside MyList class"""
+
 
 class MyList(list):
     """this is a child of list class"""
+
     def print_sorted(self):
+        """prints the list in sorted way ascendingly"""
         print(sorted(self))
