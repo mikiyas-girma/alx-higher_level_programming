e5b004824fed77c734951c6815a182e0e4d2d24b
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-05 22:46:12 +0300

    checks obj type against a_class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ def inherits_from(obj, a_class):
        (directly or indirectly) from the specified class ; otherwise False
     """
 
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class):
         return True
     else:
         return False
