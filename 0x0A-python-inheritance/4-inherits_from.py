9d0a838b35bc55431d5856245981d26116d75507
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-05 22:43:18 +0300

    issubclass() usage

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""the module contains one ``inherits_from()`` function"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that inherited
+       (directly or indirectly) from the specified class ; otherwise False
+    """
+
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
