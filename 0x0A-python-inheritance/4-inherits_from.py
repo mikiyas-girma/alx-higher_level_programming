840bcdaa8daf1d8491c4dc961b8d2c2c4ff2c9c4
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-06 00:44:27 +0300

    avoid the class in not the same as specified

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ def inherits_from(obj, a_class):
        (directly or indirectly) from the specified class ; otherwise False
     """
 
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and object.__name__ is not a_class:
         return True
     else:
         return False
