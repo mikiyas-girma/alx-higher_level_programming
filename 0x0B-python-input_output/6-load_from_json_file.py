056c6f6da45a3d3679c98b19b7315153ad90d8f8
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-07 00:52:45 +0300

    how about now

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,8 +6,6 @@ import json
 
 def load_from_json_file(filename):
     """creates an object from json file """
-    lines = []
+
     with open(filename, 'r', encoding='utf-8') as f:
-        for line in f.readlines():
-            lines.append(line)
-    return json.JSONDecoder().decode(''.join(lines))
+        return json.load(f)
