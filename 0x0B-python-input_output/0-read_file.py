0237629837d836e26bf2c62ecc3e4b31640ed194
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-06 21:35:33 +0300

    nothing should be added to the end

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """module that contains ``read_file()`` function"""
 
 
-def read_file(filename="testfile.txt"):
+def read_file(filename=""):
     """reads and prints contents in a file specified"""
     with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read())
+        print(f.read(), end='')
