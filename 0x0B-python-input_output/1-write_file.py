2387dc8a70592ad7c4cc51c2d886d878a8abdab1
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-06 21:43:29 +0300

    documented the code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""the contains one function ``write_file()``"""
+
 
 def write_file(filename="", text=""):
+    """function to write a text to a file"""
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(str(text))
