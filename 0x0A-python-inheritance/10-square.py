2f3b2fb26fc61efe65e465ee1b1d1e228d5b9dd4
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-06 12:34:55 +0300

    first validate

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,10 +6,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """inherits from the class ``Rectangle`` and instantiate it"""
     def __init__(self, size):
-        """init function used  for instantiating itself also 
+        """init function used  for instantiating itself also
             its parent class """
-        super().__init__(size, size)
         self.integer_validator('size', size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
