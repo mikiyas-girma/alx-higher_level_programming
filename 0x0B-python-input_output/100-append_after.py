0bf34f79a7733b7ecf4518a172ce0e4f24b49146
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-07 11:04:06 +0300

    search and append after key

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""contains one function ``append_after()``"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """appends a new_string to a file after a line containing
+        search_string"""
+
+    with open(filename, mode='+', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+
+        f.writelines(line_list)
