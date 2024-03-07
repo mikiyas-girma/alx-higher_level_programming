eb6942eae1e6b7a929bafda05d0b8faac49e917b
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-07 21:09:52 +0300

    added getters and setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,35 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    def get_width(self):
+        """returns width"""
+        return self.__width
+
+    def set_width(self, value):
+        """sets the width"""
+        self.__width = value
+
+    def get_height(self):
+        """returns height"""
+        return self.__height
+
+    def set_height(self, value):
+        """sets height"""
+        self.__height = value
+
+    def get_x(self):
+        """returns the value x"""
+        return self.__x
+
+    def set_x(self, value):
+        """sets the value for x"""
+        self.__x = value
+
+    def get_y(self):
+        """returns the value of y"""
+        return self.__y
+
+    def set_y(self, value):
+        """sets the value of y"""
+        self.__y = value
