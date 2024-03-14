1506408aa64c7235d4d92db81cc1ef7e50e4c06d
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-14 21:22:49 +0300

    corrected the syntax

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,9 @@
 -- Create a database with table containing foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- switch to that database
 USE hbtn_0d_usa;
--- create a table named cities
+
 CREATE TABLE IF NOT EXISTS cities(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
 	state_id INT NOT NULL FOREIGN KEY(states_id) REFERENCES states(id),
-	name VARCHAR(256) NOT NULL,
+	name VARCHAR(256) NOT NULL
 	);
