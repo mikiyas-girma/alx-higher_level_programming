83186f7e084bad134eb820a4653c73201a040029
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-14 23:58:15 +0300

    keyword capital

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- show that that have at least one genre
-select tv_shows.title, tv_show_genres.genre_id
-from tv_shows
-inner join tv_show_genres
-where tv_shows.id = tv_show_genres.show_id
-order by tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
 
