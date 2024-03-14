c7aab74ab80addb87f6f4db68fe76917ac7e3360
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-14 23:09:11 +0300

    correct joining sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- join corresponding states with their cities
-SELECT id, name, states.name 
-FROM cities NATURAL JOIN states
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id ASC;
