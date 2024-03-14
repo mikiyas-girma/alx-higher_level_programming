121e7bac7f4030b3f7ea7cd759752326b1aabe42
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-14 22:06:30 +0300

    ordered the result

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- join corresponding states with their cities
 SELECT id, name, states.name 
 FROM cities NATURAL JOIN states
-ON cities.state_id = states.id;
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
