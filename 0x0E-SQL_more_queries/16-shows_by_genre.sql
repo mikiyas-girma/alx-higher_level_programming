d05484d3191967a458f2765d04f8cbe60b0ffe8e
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-15 16:42:19 +0300

    shows by genre

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- movies by genre 
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+left JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+left JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name ASC;
