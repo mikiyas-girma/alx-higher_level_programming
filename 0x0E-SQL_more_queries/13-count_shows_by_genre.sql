67440a21601865b465409ecbaf67ebac46c539bb
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-15 12:10:07 +0300

    count number of shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- most genre type in shows
 SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY name
 ORDER BY number_of_shows DESC;
