a417abef0cedf74a10743e14e863df956b803ea1
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-15 11:42:24 +0300

    shows with no genre associated sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- result shows that do not have genre associated with
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id is NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
